8fc02332258c8c91b8192c17e8553dd5ace8feea
ViktoriiaBohach <viktoriiaperetiatko@gmail.com>
2023-02-05 14:47:51 +0200

    click method doesn't work, second commit with modified partest2.cy.js file

--- cypress/e2e/partest2.cy.js
+++ cypress/e2e/partest2.cy.js
@@ -1,29 +1,3 @@
-//  <reference types="cypress"/> 
-
-//   it('Quering elements', () => {
-//   cy.visit('https://sanitarskyi-cypress-g2.herokuapp.com/commands/assertions');
-//   cy.get('#query-btn').should('contain', 'Button');
-
-//   cy.get('li:contains("bananas")').should('contain', 'bananas');
-//   cy.contains('bananas').should('contain', 'bananas');
-//   cy.contains('li.third', 'bananas').should('contain', 'bananas');
-//   cy.get('#querying').contains('bananas').should('have.class', 'third');
-
-//   cy.get('.query-form').within( () => {
-//     cy.get('#inputEmail').should('have.attr', 'placeholder', 'Email');
-//     cy.get('#inputPassword').should('have.attr', 'placeholder', 'Password');
-
-//     // should be unreachable
-//     // cy.get('#inputName').should('have.attr', 'placeholder', 'Password');
-//   })
-
-//   cy.root().should('contain', 'bananas');
-
-//   cy.get('div.col-xs-5 form').within( () => {
-//     cy.root().should('have.class', 'query-form');
-//   })
-// })
-
 describe('Using before method', () => {
   before(()=> {
     cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com');
@@ -39,23 +13,15 @@ describe('Test suite for toaster configuration', () => {
     it('Test1', () => {
       cy.get('[data-name="chevron-down"]').click()
       .eq(0)
-
-      
-//     expect(element).to.have.text('Column content');
-//     expect(element).to.have.html('Column content');
-//     expect(element).to.contain('Column content');
 //     })
     
-//     it('Test2', () => {
-//       cy.visit('https://google.com/')
+//     it('Test2', () => {    
 //     })
     
-//     it('Test3', () => {
-//       cy.visit('https://google.com/')
+//     it('Test3', () => {  
 //     })
     
-//     it('Test4', () => {
-//       cy.visit('https://google.com/')
+//     it('Test4', () => {      
 
 //     })
     })

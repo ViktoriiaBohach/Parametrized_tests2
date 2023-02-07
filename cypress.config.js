9fb979e75277c466b539963dc172073b21764c80
ViktoriiaBohach <viktoriiaperetiatko@gmail.com>
2023-02-07 17:34:10 +0200

    unfortunately only locators were managed to find for 1 test, no assertions are wriiten to check the expected result

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  viewportHeight: 1200,
+  viewportWidth: 1600,
   e2e: {
 // e2e options here
   }
--- cypress/e2e/partest2.cy.js
+++ cypress/e2e/partest2.cy.js
@@ -1,29 +1,35 @@
 describe('Using before method', () => {
-  before(()=> {
+  beforeEach(()=> {
     cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com');
     cy.get('nb-card-header:contains("Material Light")').click();
-    cy.get('[ng-reflect-icon="menu-outline"]').click();
+    // cy.get('[ng-reflect-icon="menu-outline"]').click();
     cy.get('[class="menu-title ng-tns-c141-19"]').click();
     cy.get('[class="menu-title ng-tns-c141-23"]').click();
-    cy.get('[ng-reflect-icon="menu-outline"]').click();
-  })
+    // cy.get('[ng-reflect-icon="menu-outline"]').click();
+  });
 
 describe('Test suite for toaster configuration', () => {
+
+
   
     it('Test1', () => {
-      cy.get('[data-name="chevron-down"]').click()
-      .eq(0)
-//     })
-    
-//     it('Test2', () => {    
-//     })
-    
-//     it('Test3', () => {  
-//     })
-    
-//     it('Test4', () => {      
+      cy.get('nb-select[class="mat-ripple position-select appearance-outline size-medium status-basic shape-rectangle nb-transition"] nb-icon[icon="chevron-down-outline"]').click();
+      cy.get('#nb-option-24').click();
+      cy.get('input[name="title"]')
+        .type(' Position top right!')
+        .then( el => {
+        expect(el.val()).to.be.eq('HI there! Position top right!');
 
-//     })
-    })
+      cy.get('input[name="content"]')
+        .type(' Position top right!')
+        .then( el => {
+        expect(el.val()).to.be.eq("I'm cool toaster! Position top right!"); 
+        })
+        cy.get('nb-select[class="mat-ripple appearance-outline size-medium status-basic shape-rectangle nb-transition"]').click();
+        cy.get('#nb-option-33').click();
+        cy.get('.mat-ripple.appearance-filled.size-medium.shape-rectangle.status-basic.nb-transition').click();
+             
+  })
+})
 })
 })

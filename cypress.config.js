b0b14d0b0c4034d45e4178d814c5a7b85fe69777
ViktoriiaBohach <viktoriiaperetiatko@gmail.com>
2023-02-12 17:43:12 +0200

    2 out of 4 expected results have been covered

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 
-const { defineConfig } = require('cypress')
+const { defineConfig } = require ('cypress')
 
 module.exports = defineConfig({
   viewportHeight: 1200,
   viewportWidth: 1600,
   e2e: {
 // e2e options here
+defaultCommandTimeout:20000,
+pageLoadTimeout: 120000,
   }
 })
 
